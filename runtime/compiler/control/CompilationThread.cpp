cf38d23e01af3327686811bab280c6fe7f6c30d3
Daryl Maier <maier@ca.ibm.com>
2025-02-05 19:04:48 -0500

    Replace some uses of getLoggingEnabled()
    Either guard with an appropriate tracing option (preferred) or use
    `isEnabled_DEPRECATED()` in situations that require non-trivial
    refactoring.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- runtime/compiler/control/CompilationThread.cpp
+++ runtime/compiler/control/CompilationThread.cpp
@@ -9474,8 +9474,7 @@ TR::CompilationInfoPerThreadBase::wrappedCompile(J9PortLibrary *portLib, void * 
 
          if (debug("traceInfo") && optionSetIndex > 0)
             {
-            if (compiler->getLoggingEnabled())
-               diagnostic("Forced Option Set %d\n", optionSetIndex);
+            diagnostic("Forced Option Set %d\n", optionSetIndex);
             }
          }
       }
@@ -11022,7 +11021,7 @@ void TR::CompilationInfoPerThreadBase::logCompilationSuccess(
       if (TR::Options::_largeTranslationTime > 0 && translationTime > (uintptr_t)TR::Options::_largeTranslationTime)
          {
          OMR::Logger *log = compiler->log();
-         if (compiler->getLoggingEnabled())
+         if (log->isEnabled_DEPRECATED())
             log->printf("Compilation took %d usec\n", (int32_t)translationTime);
          compiler->dumpMethodTrees(log, "Post optimization trees for large computing method");
          }
